884c6f853d98c91f417031c34e4a073270ff7640
luong210978 <63649962+luong210978@users.noreply.github.com>
2020-12-17 09:44:06 +0700

    i

--- BookStoreClone/View/BaoCaoUC.xaml.cs
+++ BookStoreClone/View/BaoCaoUC.xaml.cs
@@ -25,7 +25,23 @@ namespace BookStoreClone.View
 
     public partial class BaoCaoUC : UserControl
     {
+        public struct sachs
+        {
+            public string tensach { get; set; }
+            public int soluongban { get; set; }
+            public int soluongnhap { get; set; }
+        }
+        private ObservableCollection<CTHD> _listCTHD;
+        private ObservableCollection<Sach> sach;
+
+        private ObservableCollection<HoaDon> _listHD;
+        private ObservableCollection<PhieuThuTien> _listPTT;
+        private ObservableCollection<CTPhieuNhap> _listCTPN;
+        private ObservableCollection<CTBaoCaoCongNo> _listCTBaoCaoCongNo;
+        private ObservableCollection<CTBaoCaoTon> _listCTBaoCaoTon;
         private List<Category> Categories { get; set; }
+        private List<sachs> n { get; set; }
+
         public BaoCaoUC()
         {
             InitializeComponent();
@@ -34,7 +50,7 @@ namespace BookStoreClone.View
             float pieWidth = 200, pieHeight = 200, centerX = pieWidth / 2, centerY = pieHeight / 2, radius = pieWidth / 2;
             mainCanvas.Width = pieWidth;
             mainCanvas.Height = pieHeight;
-
+          
             Categories = new List<Category>()
             {
                 #region test #1
@@ -167,10 +183,11 @@ namespace BookStoreClone.View
                 #endregion
 
                 #region test #6
+                
                 new Category
                 {
                     Title = "Category#01",
-                    Percentage = 20,
+					Percentage = 20,
                     ColorBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4472C4")),
                 },
 
@@ -305,14 +322,42 @@ namespace BookStoreClone.View
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
-        }       
+        }
+        int sachnhap;
+        sachs b;
+        int q;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			_listCTHD = new ObservableCollection<CTHD>(DataProvider.Ins.DB.CTHDs.Where(x => x.HoaDon.NgayBan.Value.Month == int.Parse(Thang.SelectedItem.ToString()) && x.HoaDon.NgayBan.Value.Year == int.Parse(Nam.Text.ToString())));
+            //a1 = new sachs() { tensach = 0; };
+            n = new List<sachs>();
+			 b=new sachs();
+            foreach (CTHD a in _listCTHD)
+			{
+                bool k=false;
+                for(int i=0;i<n.Count();i++) /*( var s in n)*/
+                { 
+                    if (a.Sach.TenSach == n[i].tensach)
+                    {
+                        k = true;
+      //                  n[i].soluongban += (int)a.SoLuong;
+						//n[i].soluongban += (int)a.SoLuong;
+                    }
+                    
+                }
+                //n[q].soluongban+= (int)a.SoLuong;
+                sachnhap += (int)a.SoLuong;
+			}
+		}
     }
-    public class Category
+	public class Category
     {
         public float Percentage { get; set; }
         public string Title { get; set; }
         public Brush ColorBrush { get; set; }
     }
-
+    
 }
+
 
