5e7a38975d9489236aacff35a62b9c150c1a4710
luong210978 <63649962+luong210978@users.noreply.github.com>
2020-12-30 00:38:53 +0700

    30/12

--- BookStoreClone/Model/Model2.Designer.cs
+++ BookStoreClone/Model/Model2.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\QLNS-master - Copy (2)\QLNS-master\BookStoreClone\Model\Model2.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\QLNS-master\QLNS-master\BookStoreClone\Model\Model2.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
